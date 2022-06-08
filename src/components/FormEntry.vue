a35bc2c688f1100c4b9c72fe8b2d2ca629bdc376
AnigalHub <anigal1997@gmail.com>
2022-06-08 13:19:00 +0300

    убран input про время

--- src/components/FormEntry.vue
+++ src/components/FormEntry.vue
@@ -7,7 +7,6 @@
                     <input type="text" placeholder="ФИО">
                     <input type="text" placeholder="Телефон">
                     <input type="text" placeholder="Email">
-                    <input type="text" placeholder="Выберите время нанесения">
                     <input type="text" placeholder="Выберите турнир">
                     <button>Записаться на грим</button>
                 </form>
@@ -28,17 +27,17 @@
 
 <style scoped lang="scss">
     form{
-        padding-top: 15%;
+        padding-top: 12%;
         width: 80%;
         margin: 0 auto;
     input{
-        margin-top: 16px;
+        margin-top: 1%;
         border-top: none !important;
         border-left: none !important;
         border-right: none !important;
         width: 100%;
-        font-size: 1.3rem !important;
-        font-family: 'Inter', sans-serif !important;
+        font-size: 1.45rem !important;
+        font-family: 'Forum', cursive !important;
         background: transparent;
         padding: 14px 0 15px 7px;
         color: #959DAD;
@@ -50,17 +49,17 @@
         border: none !important;
         height: 50px;
         background: #071935;
-        font-size: 1.3rem !important;
-        font-family: 'Inter', sans-serif !important;
+        font-size: 1.5rem !important;
+        font-family: 'Forum', cursive!important;
         border-radius: 8px;
         color: #FFFFFF;
         text-align: center;
     }
     input,button{
-        font-size: 16px;
+        font-size: 1.6rem;
         line-height: 21px;
 
-        font-family: 'PT Sans', sans-serif;
+        font-family: 'Forum', cursive;
     }
     }
     col{
@@ -78,8 +77,8 @@
         font-size: 2rem;
     }
     p{
-        font-size: 1.4rem;
-        font-family: 'Inter', sans-serif;
+        font-size: 1.6rem;
+        font-family: 'Forum', cursive;
         line-height: 2rem;
         font-weight: 300;margin-bottom: 6%;
     }
@@ -89,7 +88,7 @@
         color: #8792a5;
         border-radius: 7px;
         margin-top: 2%;
-        padding: 1%; font-family: 'Inter', sans-serif;
+        padding: 1%;  font-family: 'Forum', cursive;
         background: transparent;
     }
 </style>
