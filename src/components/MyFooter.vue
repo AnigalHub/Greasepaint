0c9c9fc5e2f222afef9c9cbe7290025f764c41c5
AnigalHub <anigal1997@gmail.com>
2022-06-09 14:36:00 +0300

    компонент Футер

---
+++ src/components/MyFooter.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="Myfooter">
+        <b-container>
+            <div class="text">
+                © 2022 #polyakovsteam
+            </div>
+            <div class="flex-container svgFooter">
+                <div v-for="information in ContactsRow" :key="information.text">
+                    <a :href="information.href" :target="information.target">
+
+                    </a>
+                </div>
+            </div>
+        </b-container>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Myfooter",
+        data(){
+            return{
+                ContactsRow:[
+                    {
+                        href:'tel:' + '+7 (903) 151-39-31',
+                        target:'_self'
+                    },
+                    {
+                        href:'mailto:' + 'annaignatova97@bk.ru',
+                        target:'_self'
+                    },
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .text{
+        font-size: 1.7rem;
+        font-family: 'Forum', cursive;
+        line-height: 1.8rem;
+        font-weight: 300;
+    }
+    #Myfooter{
+        padding: 2% 0;
+        background: black;
+    }
+</style>
