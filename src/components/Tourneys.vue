33530ccc1b3ab012d5f5f291f6cefaece5ddc0ca
AnigalHub <anigal1997@gmail.com>
2022-06-09 21:04:04 +0300

    изменены стили по вырисовке карточки с туром

--- src/components/Tourneys.vue
+++ src/components/Tourneys.vue
@@ -12,8 +12,8 @@
                             <div>29 октября 2022 <hr>
                                 GRAND PRIX MOSCOW<hr>
                                 Москва,
-                                Ленинский проспект, 49 стр. 3.<hr>
-                                STUDIO HALL
+                                Ленинский проспект, 49 стр. 3.
+                                ГЛАВCLUB
                             </div>
                         </b-col>
                     </b-row>
@@ -24,10 +24,10 @@
                             <img src="../../public/qe.jpg">
                         </b-col>
                         <b-col>
-                            <div>29 октября 2022 <hr>
-                                GRAND PRIX MOSCOW<hr>
+                            <div>13 ноября 2022 <hr>
+                                GRAND PRIX NBC 7<hr>
                                 Москва,
-                                Ленинский проспект, 49 стр. 3.<hr>
+                                Орджоникидзе, д. 11 стр. 1.
                                 STUDIO HALL
                             </div>
                         </b-col>
@@ -50,31 +50,30 @@
     }
     img{
         width: 100%;
-        //margin: 4% auto;
         display: block;
+        filter: brightness(80%);
     }
     h2{
-        font-size: 1.8rem;text-shadow: #293655 3px 0 120px;
+        font-size: 1.8rem;
+        text-shadow: #293655 3px 0 120px;
         font-family: 'Forum', cursive;
         margin-bottom: 3%;
         color: #8e9aaf;
     }
     .col .row{
-        margin-right: 8%;
+        background: rgba(66, 74, 90, 0.25);
+        color: #8e9aaf;
+        margin-right: 2%;
         box-shadow: 0px 1px 8px 1px #071935;
     }
-    .col .col-5{
+    .col .col-5, .col .col-4{
         padding: 4px;
     }
     .col .col{
-        padding: 1%;
-    }
-    .col .col, .col .col{
-        background: #030b1a;
+        text-align: center;
+        padding: 6% 3% 3%;
     }
     p, .col div{
-        //padding-top: 8%;
-        //text-align: center;
         font-size: 1.5rem;
         font-family: 'Forum', cursive;
         line-height: 1.8rem;
@@ -82,6 +81,6 @@
     }
     hr{
         margin: .5rem 0;
-        border-top: 1px solid #071935;
+        border-top: 1px solid #8e9aaf;
     }
 </style>
