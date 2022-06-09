206a9f4a91dfe6a668592dbeadaf187eaaec6d32
AnigalHub <anigal1997@gmail.com>
2022-06-10 02:05:44 +0300

    вынесены стили с Home

--- src/App.vue
+++ src/App.vue
@@ -51,6 +51,45 @@ export default {
     padding-left: 15px !important;
   }
 
-
+  #Home{
+    img{
+      margin-top: 2%;
+      margin-left: 10%;
+      background: radial-gradient( #293655, #1a2238, #0a0f18, #0a0f18);
+    }
+  }
+  img{
+    width: 100%;
+    height: 100%;
+  }
+  .grid{
+    display: grid;
+    grid-template-columns: 55% 45%;
+    column-gap: normal;
+  }
+  h1,p, button{
+    font-family: 'Forum', cursive;
+    font-weight: 300;
+  }
+  h1{
+    padding: 15% 0 5%;
+    font-size: 2.35rem;
+    color: #8e9aaf;
+    text-shadow: #293655 3px 0 120px;
+  }
+  p{
+    font-size: 1.8rem;
+    line-height: 2rem;
+    margin-bottom: 6%;
+  }
+  button{
+    border: 2px solid #8792a5;
+    font-size: 1.7rem;
+    color: #8e9aaf;
+    border-radius: 7px;
+    margin-top: 2%;
+    padding: 1%;
+    background: transparent;
+  }
 
 </style>
