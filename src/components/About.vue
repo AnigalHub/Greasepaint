0a06ce18e6e7da219b45ef023e907fe1bcfb5786
AnigalHub <anigal1997@gmail.com>
2022-06-11 19:52:49 +0300

    добавлена часть про бренд грима и завоеванные награды

--- src/components/About.vue
+++ src/components/About.vue
@@ -19,6 +19,26 @@
                         гримеры предоставят вам специальное средство для протирания. </p>
                 </div>
             </div>
+            <b-row>
+                <b-col>
+                    <p>Наши мастера обладают только высшей квалификацией и регулярно проходят курсы по её повышению.</p>
+                    <b-row>
+                        <b-col cols="2">
+                            <img src="../../public/78.png">
+                        </b-col>
+                        <b-col>
+                            <p>На соревнованиях мы ни раз получали награды и благодарности от руководства федерации NBC.</p>
+                        </b-col>
+                    </b-row>
+                    <p>В своей работе мы используем самые лучшие средства, подчеркивающие все достоинства тела на соревнованиях. </p>
+                    <p>Команда #polyakovsteam сотрудничает c известными брендами грима: Jan Tana и Фитнес формула</p>
+                    <img class="imgLast" src="../../public/fitnessFormula.png">
+                    <img class="imgLast" src="../../public/jantana.png">
+                </b-col>
+                <b-col cols="4">
+                    <img src="../../public/about.png">
+                </b-col>
+            </b-row>
         </b-container>
     </div>
 </template>
@@ -30,5 +50,15 @@
 </script>
 
 <style scoped>
-
+    .imgLast{
+        width: 20%;
+        float: left;
+        filter: brightness(70%);
+    }
+    .row{
+        margin-bottom: 2%;
+    }
+    /*p:first-child{*/
+    /*    //padding-top: 10% !important;*/
+    /*}*/
 </style>
