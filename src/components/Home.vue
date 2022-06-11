d6e4fb92f82f32d7c67f1c7a9cae81403a352943
AnigalHub <anigal1997@gmail.com>
2022-06-11 14:30:55 +0300

    убрано много текста - оставлено название, расшифрока + картинка

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,17 +3,16 @@
         <b-container>
             <div class="grid">
                 <div>
-                    <p>Профессиональный грим спортсменов NBC перед выступлениями</p>
-
-                    <p>Мы команда #polyakovsteam - профессиональные гримеры спортсменов.</p>
-                    <p>Качественный грим - это залог эффектного выступления в соревнованиях по бодибилдингу и фитнесу.
-                    Под яркими софитами грим выделяет мышцы и подчеркивает рельеф тела.
-                    </p>
-                    <p>Наша цель - ваш идеально красивый путь к победе.</p>
-                    <button class="floating-button">Подробнее...</button>
+                    <p>Мы команда <br/> #polyakovsteam
+                    <p>Профессиональные гримеры спортсменов NBC перед выступлениями</p>
+<!--                    <p><b>Качественный грим </b> - это залог эффектного выступления в соревнованиях по бодибилдингу и фитнесу.-->
+<!--                    Под яркими софитами грим выделяет мышцы и подчеркивает рельеф тела.-->
+<!--                    </p>-->
+<!--                    <p>Наша цель - ваш идеально красивый путь к победе.</p>-->
+                    <button class="floating-button">Узнать больше</button>
                 </div>
                 <div>
-                    <img src="../../public/home.png">
+                    <img src="../../public/99.png">
                 </div>
             </div>
         </b-container>
@@ -21,11 +20,21 @@
 </template>
 
 <script>
+    import AirBrush_svg from "./svg_home/airbrush";
     export default {
-        name: "Home"
+        name: "Home",
+        data(){
+            return{
+                svg:AirBrush_svg
+            }
+        }
     }
 </script>
 
 <style scoped lang="scss">
-
+img{
+    padding-top: 4%;
+    width: 102%;
+    margin: 0 auto;
+}
 </style>
