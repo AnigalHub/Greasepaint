26983b42c63d4fe1f16428e898d7b33673d9b43a
AnigalHub <anigal1997@gmail.com>
2022-06-11 14:19:12 +0300

    добавлены row и col

--- src/components/FormEntry.vue
+++ src/components/FormEntry.vue
@@ -1,14 +1,20 @@
 <template>
     <div id="FormEntry">
         <b-container>
-            <form>
-                <input type="text" placeholder="ФИО">
-                <input type="text" placeholder="Телефон">
-                <input type="text" placeholder="Email">
-                <input type="text" placeholder="Выберите турнир">
-                <button>Записаться на грим</button>
-            </form>
-            <p>Напоминаем, что на грим надо записываться уже сейчас! Не подводите себя и других спортсменов, не оставляйте на последний день.</p>
+            <b-row>
+                <b-col>
+                    <form>
+                        <input type="text" placeholder="ФИО">
+                        <input type="text" placeholder="Телефон">
+                        <input type="text" placeholder="Email">
+                        <input type="text" placeholder="Выберите турнир">
+                        <button>Записаться на грим</button>
+                    </form>
+                </b-col>
+                <b-col cols="4">
+                    <p>Напоминаем, что на грим надо записываться уже сейчас! Не подводите себя и других спортсменов, не оставляйте на последний день.</p>
+                </b-col>
+            </b-row>
         </b-container>
     </div>
 </template>
