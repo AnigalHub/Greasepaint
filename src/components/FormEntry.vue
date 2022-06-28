17fb19b343aa3fa9c8b81f6b87c8a978d67ffd5d
AnigalHub <anigal1997@gmail.com>
2022-06-28 18:48:57 +0300

    изменена форма записи + добавлена картинка

--- src/components/FormEntry.vue
+++ src/components/FormEntry.vue
@@ -6,18 +6,21 @@
                 <p>Напоминаем, что на грим надо записываться уже сейчас!
                     Не подводите себя и других спортсменов, не оставляйте на последний день.</p>
                 <b-row>
+                    <b-col cols="5">
+                        <img src="../../public/ww.png">
+                    </b-col>
                     <b-col>
+
                         <form>
                             <input type="text" placeholder="ФИО">
                             <input type="text" placeholder="Телефон">
                             <input type="text" placeholder="Email">
                             <input type="text" placeholder="Выберите турнир">
+                            <input type="text" placeholder="Выберите время">
                             <button>Записаться</button>
                         </form>
                     </b-col>
-                    <b-col cols="6">
 
-                    </b-col>
                 </b-row>
             </div>
 
