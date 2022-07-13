df556d33736962da6f9944749d32e8699d85eafd
AnigalHub <anigal1997@gmail.com>
2022-07-13 23:26:40 +0300

    добавлен массив Tourneys

--- src/components/Tourneys.vue
+++ src/components/Tourneys.vue
@@ -41,7 +41,25 @@
 
 <script>
     export default {
-        name: "Tourneys"
+        name: "Tourneys",
+        data(){
+            return{
+                Tourneys:[
+                    {
+                        src: '../../public/qw.jpg',
+                        date:'29 октября 2022',
+                        name:'GRAND PRIX MOSCOW',
+                        address:'Москва, Ленинский проспект, 49 стр. 3. ГЛАВCLUB'
+                    },
+                    {
+                        src: '../../public/qe.jpg',
+                        date:'13 ноября 2022',
+                        name:'GRAND PRIX NBC 7',
+                        address:'Москва, Орджоникидзе, д. 11 стр. 1. STUDIO HALL'
+                    },
+                ],
+            }
+        }
     }
 </script>
 
