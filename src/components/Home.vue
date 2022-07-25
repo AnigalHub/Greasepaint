f188a8012457e1664e68e7d63c2ae6332f20dad4
AnigalHub <anigal1997@gmail.com>
2022-07-25 21:44:04 +0300

    обавлен заголовок - h1

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,8 +2,8 @@
     <div id="Home">
         <b-container>
             <b-row>
-                <b-col cols="5">
-                    <p>Мы команда <br/> #polyakovsteam
+                <b-col cols="4">
+                    <h1>Мы команда <br/> #polyakovsteam</h1>
                     <p>Профессиональные гримеры спортсменов NBC перед выступлениями</p>
                     <p><a href="">Узнать больше</a></p>
                 </b-col>
