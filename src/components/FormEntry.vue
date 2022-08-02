f14b39b54bc7fb27160cde4c05919ddf77244277
AnigalHub <anigal1997@gmail.com>
2022-08-02 21:31:59 +0300

    предварительная стоимость до кнопки

--- src/components/FormEntry.vue
+++ src/components/FormEntry.vue
@@ -8,17 +8,19 @@
                         <div class="imgBg"></div>
                     </b-col>
                     <b-col>
+
                         <form>
                             <h2>Запись на грим</h2>
-                            <b-form-input v-model="formEntry.name" type="text" placeholder="ФИО"/>
-                            <b-form-input v-model="formEntry.phone" type="tel" placeholder="Телефон"/>
-                            <b-form-select v-model="selected" :options="Tourneys" placeholder="Выберите турнир"/>
-                            <b-form-input v-model="formEntry.time" type="time" placeholder="Выберите время"/>
+                            <div>
+                                <b-form-input v-model="formEntry.name" type="text" placeholder="ФИО"/>
+                                <b-form-input v-model="formEntry.phone" type="tel" placeholder="Телефон"/>
+                                <b-form-select v-model="selected" :options="Tourneys" placeholder="Выберите турнир"/>
+                                <b-form-input v-model="formEntry.time" type="time" placeholder="Выберите время"/>
+                                <p class="aboutMoney">Предварительная стоимость:
+                                    <span class="money">2500 руб.</span></p>
+                            </div>
                             <button>Записаться</button>
                         </form>
-                        <p class="aboutMoney">Предварительная стоимость:
-                            <span class="money">2500 руб.</span></p>
-                        <hr/>
                         <p>Напоминаем, что на грим надо записываться уже сейчас!
                             Не подводите себя и других спортсменов, не оставляйте на последний день.</p>
                     </b-col>
