edc5d566fed465b55118529b99443dfb94f01493
AnigalHub <anigal1997@gmail.com>
2022-08-07 00:21:23 +0300

    добавлены стили по дате, верстка через flex-container

--- src/components/Tourneys.vue
+++ src/components/Tourneys.vue
@@ -2,20 +2,21 @@
     <div id="Tourneys">
         <b-container>
             <h2>Ближайшие турниры федерации NBC:</h2>
-            <b-row class="tourney">
-                <b-col v-for="tourney in Tourneys">
+            <div class="flex-container" >
+                <div v-for="tourney in Tourneys" class="tou">
+                    <p class="date">{{tourney.date}}</p>
                     <b-row>
                         <b-col cols="4">
                             <img :src="tourney.src" :alt="tourney.name">
                         </b-col>
                         <b-col>
-                            <p>{{tourney.date}}</p><hr>
-                            <p>{{tourney.name}}</p><hr>
+                            <p>{{tourney.name}}</p>
                             <p> {{tourney.address}}</p>
                         </b-col>
                     </b-row>
-                </b-col>
-            </b-row>
+
+                </div>
+            </div>
         </b-container>
     </div>
 </template>
@@ -45,5 +46,16 @@
 </script>
 
 <style scoped lang="scss">
-
+.date{
+    background: rgba(24, 57, 80, 0.3);
+    padding: 2% 4%;
+    color: #3c88b5;
+}
+.flex-container > div {
+    width: 45%;
+    margin: 0 1.5% 2.5% 1.5% !important;
+    background: rgba(24, 57, 80, 0.3);
+    border: 2px solid rgba(67, 103, 167, 0.21);
+    border-radius: 8px;
+}
 </style>
