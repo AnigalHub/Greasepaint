d0a91b2bd8bf9ce3c484a8c0ee424e65828abf95
AnigalHub <anigal1997@gmail.com>
2022-08-14 09:02:32 +0300

    добавлены ссылки

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,8 +6,8 @@
                     <h1>Мы команда #polyakovsteam</h1>
                     <p>Профессиональные гримеры спортсменов NBC перед выступлениями</p>
                     <div class="allButton">
-                        <b-button>Запись на грим</b-button>
-                        <b-button>Узнать больше</b-button>
+                        <b-button><a @click="scrollToId" href="#FormEntry">Запись на грим</a></b-button>
+                        <b-button><a @click="scrollToId" href="#About">Узнать больше</a></b-button>
                     </div>
                 </b-col>
                 <b-col>
@@ -22,6 +22,27 @@
 <script>
     export default {
         name: "Home",
+        data(){
+            return {
+                isExpanded: false,
+            }
+        },
+        methods:{
+            scrollToTop(){
+                window.scrollTo({top: 0, left: 0, behavior: 'smooth' });
+            },
+            scrollToId(event){
+                let offsetY = 50;
+                if(this.isExpanded){
+                    let nav = document.getElementById(("Nav"));
+                    let bounds =  nav.getBoundingClientRect();
+                    offsetY = bounds.height;
+                }
+                let element = document.getElementById(event.target.hash.replace("#", ""));
+                window.scrollTo({top: element.offsetTop - offsetY, left: 0, behavior: 'smooth'});
+                event.preventDefault();
+            }
+        }
     }
 </script>
 
