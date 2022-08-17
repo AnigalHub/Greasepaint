9ec24b5e3fd0089449c668ed3d2b0d0fc95579fa
AnigalHub <anigal1997@gmail.com>
2022-08-17 09:13:55 +0300

    импорт миксина, удаление кода, которые в миксине

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -25,29 +25,10 @@
 
 
 <script>
+    import { scroll } from '@/mixins/scroll';
     export default {
         name: "navbar",
-        data(){
-            return {
-                isExpanded: false,
-            }
-        },
-        methods:{
-            scrollToTop(){
-                window.scrollTo({top: 0, left: 0, behavior: 'smooth' });
-            },
-            scrollToId(event){
-                let offsetY = 50;
-                if(this.isExpanded){
-                    let nav = document.getElementById(("Nav"));
-                    let bounds =  nav.getBoundingClientRect();
-                    offsetY = bounds.height;
-                }
-                let element = document.getElementById(event.target.hash.replace("#", ""));
-                window.scrollTo({top: element.offsetTop - offsetY, left: 0, behavior: 'smooth'});
-                event.preventDefault();
-            }
-        }
+        mixins:[scroll],
     }
 </script>
 
