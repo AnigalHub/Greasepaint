50cf3cd81ea21243c6d0c28aae342da0502f9dc0
AnigalHub <anigal1997@gmail.com>
2022-08-18 09:14:46 +0300

    импорт миксина на скролл

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -20,29 +20,11 @@
 </template>
 
 <script>
+    import {scroll} from "@/mixins/scroll";
+
     export default {
         name: "Home",
-        data(){
-            return {
-                isExpanded: false,
-            }
-        },
-        methods:{
-            scrollToTop(){
-                window.scrollTo({top: 0, left: 0, behavior: 'smooth' });
-            },
-            scrollToId(event){
-                let offsetY = 50;
-                if(this.isExpanded){
-                    let nav = document.getElementById(("Nav"));
-                    let bounds =  nav.getBoundingClientRect();
-                    offsetY = bounds.height;
-                }
-                let element = document.getElementById(event.target.hash.replace("#", ""));
-                window.scrollTo({top: element.offsetTop - offsetY, left: 0, behavior: 'smooth'});
-                event.preventDefault();
-            }
-        }
+        mixins:[scroll],
     }
 </script>
 
