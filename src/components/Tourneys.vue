2a1f9072d819dfc6de8b22320a8a79f84d446388
AnigalHub <anigal1997@gmail.com>
2022-08-31 19:00:16 +0300

    изменен computed, добавлен метод на подсчет номера месяца в массиве

--- src/components/Tourneys.vue
+++ src/components/Tourneys.vue
@@ -1,9 +1,9 @@
 <template>
-    <div id="Tourneys" v-if="dateTouneys">
+    <div id="Tourneys" v-if="tourneysToShow.length > 0">
         <b-container>
             <h2>Ближайшие турниры федерации NBC:</h2>
-            <div class="flex-container" v-show="dateСomparison">
-                <div v-for="tourney in Tourneys">
+            <div class="flex-container">
+                <div v-for="tourney in tourneysToShow" >
                     <p class="date" >{{tourney.date}}</p>
                     <b-row>
                         <b-col cols="4">
@@ -26,7 +26,53 @@
         data(){
             return{
                 today: new Date(),
-                arrayMonths :  [
+                dateTourneys:false,
+                Tourneys:[
+                    {
+                        src: "./qw.jpg",
+                        date:"29 октября 2022",
+                        name:"GRAND PRIX MOSCOW",
+                        address:"Москва, Ленинский проспект, 49 стр. 3. ГЛАВCLUB",
+                    },
+                    {
+                        src: "./qe.jpg",
+                        date:"13 декабря 2022",
+                        name:"GRAND PRIX NBC 7",
+                        address:"Москва, Орджоникидзе, д. 11 стр. 1. STUDIO HALL"
+                    },
+                ],
+            }
+        },
+        computed:{
+            tourneysToShow(){
+                let tournaments = []
+
+                for (let i =0; i < this.Tourneys.length; i++){
+                    let date = this.Tourneys[i].date;
+                    console.log('date', date)
+                    let day = date.substr(0, 2)
+                    let year = (date.substring(date.length - 4));
+                    let month = this.SearchForTheNumberOfTheMonth(date,day,year);
+                    console.log(day,month,year)
+                    const dataTourney = new Date(Number(year),month,Number( day));
+
+                    let dm = this.today - dataTourney;
+                    console.log('dm', dm)
+                    const ONE_DAY_IN_MS = 86400000;
+                    if ( dm < ONE_DAY_IN_MS ){
+                        tournaments.push(this.Tourneys[i])
+                    }
+
+                }
+                return tournaments;
+            },
+
+
+
+        },
+        methods:{
+            SearchForTheNumberOfTheMonth(date,day,year){
+                const arrayMonths =  [
                     'января',
                     'февраля',
                     'марта',
@@ -39,54 +85,16 @@
                     'октября',
                     'ноября',
                     'декабря'
-                ],
-                dateTouneys:true,
-                Tourneys:[
-                    {
-                        src: "./qw.jpg",
-                        date:"29 октября 2022",
-                        name:"GRAND PRIX MOSCOW",
-                        address:"Москва, Ленинский проспект, 49 стр. 3. ГЛАВCLUB"
-                    },
-                    {
-                        src: "./qe.jpg",
-                        date:"13 ноября 2022",
-                        name:"GRAND PRIX NBC 7",
-                        address:"Москва, Орджоникидзе, д. 11 стр. 1. STUDIO HALL"
-                    },
-                ],
-            }
-        },
-        computed:{
-            dateСomparison(){
-                let date =  this.Tourneys[0].date
-                console.log('date', this.Tourneys[0].date)
-                let day = date.substr(0, 2)
-                let year = (date.substring(date.length - 4));
-
+                ]
                 //удаление числа
                 var newstr = date.replace(day, "");
                 //удаление года
                 newstr = newstr.replace(year, "")
                 //удаление пробелов
                 newstr = newstr.split(' ').join('')
-
-
-                let month = this.arrayMonths.indexOf(newstr);
-
-
-                const dataTourney = new Date(Number(year),month,Number( day));
-
-                const dm = this.today - dataTourney;
-
-                if ( dm > 86400000 ){
-                    this.dateTouneys = false;
-                    console.log('текущая дата больше даты турнира')
-                }
-                return this.dateTouneys;
-
+                let month = arrayMonths.indexOf(newstr);
+                return month;
             }
-
         }
     }
 </script>
