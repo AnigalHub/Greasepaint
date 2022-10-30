0d58bd9011fa0f31140cb1e9e4107849e5f15b22
AnigalHub <anigal1997@gmail.com>
2022-10-30 15:47:37 +0300

    изменена верстка формы

--- src/components/FormEntry.vue
+++ src/components/FormEntry.vue
@@ -1,21 +1,26 @@
 <template>
     <div id="FormEntry">
         <b-container>
-            <b-row>
-                <b-col>
-                    <form>
-                        <input type="text" placeholder="ФИО">
-                        <input type="text" placeholder="Телефон">
-                        <input type="text" placeholder="Email">
-                        <input type="text" placeholder="Выберите турнир">
-                        <button>Записаться на грим</button>
-                    </form>
-                </b-col>
-                <b-col cols="4">
-                    <p>Напоминаем, что на грим надо записываться уже сейчас! </p>
-                    <p>Не подводите себя и других спортсменов, не оставляйте на последний день.</p>
-                </b-col>
-            </b-row>
+            <div class="block">
+                <h2>Запись на грим</h2>
+                <p>Напоминаем, что на грим надо записываться уже сейчас!
+                    Не подводите себя и других спортсменов, не оставляйте на последний день.</p>
+                <b-row>
+                    <b-col>
+                        <form>
+                            <input type="text" placeholder="ФИО">
+                            <input type="text" placeholder="Телефон">
+                            <input type="text" placeholder="Email">
+                            <input type="text" placeholder="Выберите турнир">
+                            <button>Записаться</button>
+                        </form>
+                    </b-col>
+                    <b-col cols="6">
+
+                    </b-col>
+                </b-row>
+            </div>
+
         </b-container>
     </div>
 </template>
