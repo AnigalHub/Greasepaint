84de697101f2a1c5c07ec0299726e85e06dc6131
AnigalHub <anigal1997@gmail.com>
2022-11-16 20:08:17 +0300

    изменен нейминг в computed и methods

--- src/components/Tournaments.vue
+++ src/components/Tournaments.vue
@@ -48,16 +48,16 @@
 
                 this.Tournaments.forEach((tournament) =>{
                     let date = (tournament.date).split(" ");
-                    let day = date[0]
+                    let day = date[0];
                     let year = date[2];
                     let month = this.SearchForTheNumberOfTheMonth(date[1]);
 
                     const dateTournament = new Date(Number(year),month,Number( day));
 
-                    let dm = this.today - dateTournament;
+                    let dateDifference = this.today - dateTournament;
 
                     const ONE_DAY_IN_MS = 86400000;
-                    if ( dm < ONE_DAY_IN_MS ){
+                    if ( dateDifference < ONE_DAY_IN_MS ){
                         tournaments.push(tournament)
                     }
                 })
@@ -65,23 +65,22 @@
             },
         },
         methods:{
-            SearchForTheNumberOfTheMonth(newstr){
-                const arrayMonth ={
-                    января : 0,
-                    февраля : 1,
-                    марта : 2,
-                    апреля : 3,
-                    мая : 4,
-                    июня : 5,
-                    июля : 6,
-                    августа : 7,
-                    сентября : 8,
-                    октября : 9,
-                    ноября : 10,
-                    декабря : 11
+            SearchForTheNumberOfTheMonth: function (monthStr) {
+                const months = {
+                    января: 0,
+                    февраля: 1,
+                    марта: 2,
+                    апреля: 3,
+                    мая: 4,
+                    июня: 5,
+                    июля: 6,
+                    августа: 7,
+                    сентября: 8,
+                    октября: 9,
+                    ноября: 10,
+                    декабря: 11
                 }
-                let month = arrayMonth[newstr];
-                return month;
+                return months[monthStr];
             }
         }
     }
