9d50a693eb8af8a9604a6b9b004dea4840dec0d0
AnigalHub <anigal1997@gmail.com>
2023-04-11 21:57:20 +0300

    добавлен компонент про награды и благодарности

---
+++ src/components/Awards.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="Awards">
+        <b-container>
+            <h2>Награды и благодарности команды:</h2>
+            <b-row>
+                <b-col cols="2">
+                    <img src="../../public/2.png" alt="Нанесение грима">
+                </b-col>
+                <b-col>
+                    <p class="awards text">
+                        Выражаем благодарность команде гримеров #POLYAKOVSTEAM
+                        за профессиональную работу, вклад в организацию турниров
+                        и развитие National Bodybuilding Community.
+                    </p>
+                    <br>
+                    <p class="author text">Федерация National Bodybuilding Community.</p>
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Awards",
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
