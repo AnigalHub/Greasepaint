48f8e73769eece87a86760b1e23fb5e7e01d257a
AnigalHub <anigal1997@gmail.com>
2023-07-13 03:18:55 +0300

    изменен alt у картинки, удален класс css - который не используется

--- src/components/Awards.vue
+++ src/components/Awards.vue
@@ -4,12 +4,12 @@
             <h2>Награды и благодарности команды</h2>
             <b-row>
                 <b-col cols="2">
-                    <img src="../../public/awards/awards.png" alt="Нанесение грима">
+                    <img src="../../public/awards/awards.png" alt="Награды">
                 </b-col>
                 <b-col>
-                    <p class="awards text">Выражаем благодарность команде гримеров #POLYAKOVSTEAM за профессиональную работу, вклад в организацию турниров и развитие National Bodybuilding Community.</p>
+                    <p class="awards">Выражаем благодарность команде гримеров <span>#polyakovsteam</span> за профессиональную работу, вклад в организацию турниров и развитие National Bodybuilding Community.</p>
                     <br>
-                    <p class="author text">Федерация National Bodybuilding Community.</p>
+                    <p class="author">Федерация National Bodybuilding Community.</p>
                 </b-col>
             </b-row>
         </b-container>
