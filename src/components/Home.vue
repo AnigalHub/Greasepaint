2f24749f3f3e3de53467fb7414b6fd9898738bf4
AnigalHub <anigal1997@gmail.com>
2023-07-14 03:19:45 +0300

    удаление блока imgBg, убран блок под кнопку

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,21 +2,17 @@
     <div id="Home">
         <b-container>
             <b-row>
-                <b-col cols="4">
+                <b-col cols="2">
                     <b-row>
                         <b-col>
-                            <h1 class="css-typing">Мы команда <br/><span>{{name}}</span></h1>
-                            <p>Профессиональные гримеры спортсменов на соревнованиях
-                                National Bodybuilding Community по бодибилдингу и фитнесу</p>
-                            <div class="blockButton">
-                                <b-button><a @click="scrollToId" href="#FormEntry">Запись на грим</a></b-button>
-                            </div>
+                            <h1><span>Подчеркни рельеф своего тела!</span></h1>
+                            <p><span>Профессиональный грим спортсменов на соревнованиях National Bodybuilding Community по бодибилдингу и фитнесу</span></p>
+                            <b-button><a @click="scrollToId" href="#FormEntry">Запись на грим</a></b-button>
                         </b-col>
                     </b-row>
                 </b-col>
                 <b-col>
-                    <img src="../../public/componentsImg/home.png" :alt="name">
-                    <div class="imgBg"></div>
+                    <img src="../../public/componentsImg/home.png" alt="Главная">
                 </b-col>
             </b-row>
         </b-container>
@@ -24,16 +20,10 @@
 </template>
 
 <script>
-    import information from '../../public/documents/information.json';
     import {scroll} from "@/mixins/scroll";
     export default {
         name: "Home",
         mixins:[scroll],
-        data(){
-            return{
-                name:information.name_company
-            }
-        }
     }
 </script>
 
